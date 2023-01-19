6ed24f39d2b731e1906a2995689656d7d8b9d04f
Fatima <110629449+fatimagulomova@users.noreply.github.com>
2023-01-19 22:36:32 +0500

    Update test_db.py

--- habit-tracking-application/test_db.py
+++ habit-tracking-application/test_db.py
@@ -5,20 +5,18 @@ class TestHabit:
 
     def test_add_new_habit(self):
         habit = AddHabit()
-        habit.add_habit(name='python_class', category='study', frequency='daily', duration='6 weeks', unitset='2 hours',
-                        start_date='2022/12/15', file_db="test.db")
+        habit.add_habit(name='piano class', category='hobby', frequency='weekly', duration='15 months',
+                        file_db="files/predefined-habits.db")
 
     def test_edit_habit(self):
-        habit = EditHabit(habit_name="python_class", choice="unitset", new_value="2.5 hours")
-        habit.edit(file_db="test.db")
+        habit = EditHabit(habit_name="programming", choice="duration", new_value="6 months")
+        habit.edit(file_db="files/predefined-habits.db")
 
     def test_analyse_habit(self):
         habit = AnalyzeHabit()
-        habit.check_off(name="python_class", answer="yes", file_db="test.db")
-        habit.report(purpose='all habits', file_db="test.db")
+        habit.check_off(name="programming", file_db="files/predefined-habits.db")
+        habit.report(purpose='all habits', file_db="files/predefined-habits.db")
 
     def test_delete(self):
         habit = DeleteHabit()
-        habit.delete_the_habit(name="python_class", file_db="test.db")
-        import os
-        os.remove("test.db")
+        habit.delete_the_habit(name="no_coffee", file_db="files/predefined-habits.db")
